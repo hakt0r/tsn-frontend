169597e59341fc39eda98de5699a020b098a3b18
Sebastian Glaser <hakt0r@users.noreply.github.com>
2020-12-15 04:30:31 +0100

    cleanup profile

---
+++ src/Profile/Friends.js
@@ -0,0 +1,57 @@
+
+import React           from 'react';
+import { useSelector } from 'react-redux';
+
+import Avatar     from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
+
+import { useUser } from '../Data/hooks';
+
+import FlexGrow    from '../Layout/FlexGrow';
+
+import { makeStyles }  from "@material-ui/core/styles";
+import { paperTheme }  from "../styles";
+import { IconButton } from '@material-ui/core';
+
+import Show from '../Layout/Show';
+import { addFriend, rejectFriend } from './actions';
+import { HighlightOff, PersonAddDisabled, ThumbDown, ThumbUp } from '@material-ui/icons';
+import FlexRow from '../Layout/FlexRow';
+
+const useStyles = makeStyles( paperTheme );
+
+function Friend({id,incoming,outgoing}) {
+  const friend  = useUser(id);
+  const classes = useStyles();
+  return (
+  <FlexRow key={friend.id} className={classes.row}>
+    <Avatar src={friend.avatar}/>
+    <Typography variant="h6" style={{marginLeft:'1ch'}}>
+      {friend.name}
+    </Typography>
+    <FlexGrow/>
+    <IconButton onClick={e=>rejectFriend(friend.id)}>
+      { incoming
+      ? <ThumbDown color="secondary"/>
+      : outgoing
+      ? <HighlightOff color="secondary"/>
+      : <PersonAddDisabled color="secondary"/> } 
+    </IconButton>
+    <Show when={incoming}>
+      <IconButton onClick={e=>addFriend(friend.id)}>
+        <ThumbUp color="primary"/>
+      </IconButton>
+    </Show>
+  </FlexRow> );
+}
+
+export default function Friends () {
+  const userId = useSelector( s => s.auth.user.id );
+  const user   = useUser( userId );
+  if ( ! user.friends ) return null;
+  return ( <>
+  { user.friends.map( friend => <Friend id={friend.id||friend}/> )}
+  { user.friendRequests.map( friend => <Friend incoming id={friend.id||friend}/> )}
+  { user.friendRequestsSent.map( friend => <Friend outgoing id={friend.id||friend}/> )}
+  </> );
+}
